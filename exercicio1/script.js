d18faf7885a0b9f3206ab01aa388a605caadaf2b
Felipeferrazamaral <felipeferrazamaral@gmail.com>
2023-02-01 13:56:00 -0300

    exercicio de fixacao pronto

---
+++ exercicio1/script.js
@@ -0,0 +1,25 @@
+// parte 1
+//const nome = prompt('Qual é o seu nome?')
+//console.log(typeof nome)
+
+//let idade = prompt('Qual sua idade?')
+
+//console.log(typeof idade)
+// nao definido por nao ter valor atribuido as variaveis
+
+// as duas variaveis estao como string
+
+//console.log('ola',nome,'voce tem',idade,'anos.')
+
+// parte 2
+const perguntaA = 'voce eh santista?'
+const respostaA = prompt(perguntaA)
+console.log(perguntaA , respostaA)
+
+const perguntaB = 'voce gosta de sorvete?'
+const respostaB = prompt(perguntaB)
+console.log(perguntaB , respostaB)
+
+const perguntaC = 'voce surfa?'
+const respostaC = prompt(perguntaC)
+console.log(perguntaC , respostaC)
